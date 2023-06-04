7d0dbb27c690214e7d27f44d2e7f4a44a87ba009
Pager-dot <109455621+Pager-dot@users.noreply.github.com>
2023-06-05 01:08:28 +0530

    changed a few names

--- Text extraction.py
+++ Text extraction.py
@@ -1,19 +1,19 @@
 import rumps
 import subprocess
 
-class Screen_time(rumps.App):
+class Utility(rumps.App):
     def __init__(self):
-        super(Screen_time, self).__init__("Screen Time")
-        self.menu = ["Usage","Quit"]
+        super(Utility, self).__init__("--_--")
+        self.menu = ["Text Extraction","Quit"]
         self._quit_button =None
 
-    @rumps.clicked("Usage")
+    @rumps.clicked("Text Extraction")
     def prefs(self,_):
         subprocess.run("screencapture -ci ~/Desktop/screenshot.jpg",shell=True)
-        subprocess.run("/usr/bin/python3 /Users/paritosh/Desktop/CS/Screen-usage-widget-main/1.py",shell=True)
+        subprocess.run("/usr/bin/python3 /Users/paritosh/Documents/GitHub/Text-Extractor-for-mac-os/module1.py",shell=True)
 
     @rumps.clicked('Quit')
-    def quit_Screen_time(self,_):
+    def quit_Utility(self,_):
         rumps.quit_application()
 
-Screen_time().run()
+Utility().run()
